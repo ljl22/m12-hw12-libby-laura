12744f33cb83a319efcaddab33dafddd3a383c42
ljl22 <lauraj.libby@gmail.com>
2019-11-08 15:11:19 -0500

    Update includeSpoon method in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-//classes are object making machines
-// create parent class
-
+//classes are object making machines, create parent class
 class Desserts {
   constructor(type, calories) {
     this.name = type;
@@ -14,9 +12,12 @@ class IceCream extends Desserts {
     super(type, calories);
     this.flavor = flavor;
     this.scoops = scoops;
+    //this.includeSpoon(console.log('Here is your spoon!')) = include;
+    this.includeSpoon = function () {
+      console.log('Here is your spoon!');
   }
 }
 
 //give above properities a value by creating a variable (not creating another class)
-const vanillaIceCream = new VanillaIceCream('ice cream', '340', 'vanilla', '3');
+const vanillaIceCream = new IceCream('ice cream', '340', 'vanilla', '3');
 console.log(vanillaIceCream);
