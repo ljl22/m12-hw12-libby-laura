3e6bd2b6cc4b63fa3e5c56258e745f372466ef10
ljl22 <lauraj.libby@gmail.com>
2019-11-08 14:49:12 -0500

    Add in desert javascript parent and child classes to main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
+//classes are object making machines
+// create parent class
 
+class Desserts {
+  constructor(type, calories) {
+    this.name = type;
+    this.calories = calories;
+  }
+}
+
+// create child class ClassName
+class IceCream extends Desserts {
+  constructor(type, calories, flavor, scoops) {
+    super(type, calories);
+    this.flavor = flavor;
+    this.scoops = scoops;
+  }
+}
+
+//give above properities a value by creating a variable (not creating another class)
+const vanillaIceCream = new VanillaIceCream('ice cream', '340', 'vanilla', '3');
